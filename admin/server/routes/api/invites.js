0c963da3c7b437cbeee057e102db2bca3239a499
Linus Miller <lohfu@lohfu.io>
2015-08-14 11:30:29 +0200

    Disable put (update) routes for permissions, roles and invites.

--- admin/server/routes/api/invites.js
+++ admin/server/routes/api/invites.js
@@ -2,7 +2,7 @@ module.exports = function (mw) {
 	return [
 		[ 'get', '/', mw.api.invites.findAll ],
 		[ 'post', '/', mw.api.invites.create ],
-		[ 'put', '/:id', mw.api.invites.update ],
+		//[ 'put', '/:id', mw.api.invites.update ],
 		[ 'delete', '/:id', mw.api.invites.remove ]
 	];
 };
--- admin/server/routes/api/roles.js
+++ admin/server/routes/api/roles.js
@@ -2,7 +2,7 @@ module.exports = function(mw, config) {
 	return [
 		[ 'get', '/', mw.api.roles.findAll ],
 		[ 'post', '/', mw.api.roles.create ],
-		[ 'put', '/:id', mw.api.roles.update ],
+		//[ 'put', '/:id', mw.api.roles.update ],
 		[ 'delete', '/:id', mw.api.roles.remove ]
 	];
 };
