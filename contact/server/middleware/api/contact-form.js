d6ca0f85672adc9c505d2ca5a96b099cb6a79400
Linus Miller <lohfu@lohfu.io>
2015-09-15 16:15:20 +0200

    Tried to make contacts a little more generic.

--- contact/server/middleware/api/contact-form.js
+++ contact/server/middleware/api/contact-form.js
@@ -3,33 +3,21 @@ module.exports = function(config, mongoose) {
 
 	return function (req, res, next) {
 		var smtpTransport = nodemailer.createTransport(config.mail);
-		console.log(req.body);
 
 		res.render('emails/contact-form', req.body, function (err, html) {
 			smtpTransport.sendMail({
-				from: config.mail.emails.robot,
+				from: config.contactForm.from,
 				replyTo: req.body.email,
-				to: 'linus.miller@thecodebureau.com',
-				subject: 'Message from website visitor',
+				to: config.contactForm.to,
+				subject: config.contactForm.subject,
 				html: html
 			}, function (err) {
 				// TODO try to get a hold page object
-				if(!req.xhr)
-					res.locals.template = 'pages/kontakt';
-
 				if (err) {
-					res.data.formStatus = {
-						type: 'error',
-						heading: 'Hoppsan!',
-						body: 'Det här tyvärr upstått ett problem med att skicka ditt meddelande. Om problemet kvarstår prova gärna att kontakta oss på annat sätt.'
-					};
+					res.data.formStatus = config.contactForm.error;
 					res.status(500);
 				} else {
-					res.data.formStatus = {
-						type: 'success',
-						heading: 'Meddelande skickat!',
-						body: 'Vi kommer att besvara på er förfrågan så fort som möjligt! Tack för er tid!'
-					};
+					res.data.formStatus = config.contactForm.success;
 				}
 
 				next();
