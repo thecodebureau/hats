96e64a93bbd8415c8fa877dead7d9a4de807af74
Linus Miller <lohfu@lohfu.io>
2015-09-28 19:29:28 +0200

    Removed console.logs.

--- content/browser/admin/views/content-page.js
+++ content/browser/admin/views/content-page.js
@@ -2,17 +2,10 @@ var app = require('ridge');
 
 module.exports = require('ridge/view').extend({
 	subviews: {
-		//CrudForm: '.form',
 		CrudCollection: {
 			selector: '.collection',
 			collection: 'Fields',
 			modelTemplate: 'admin/models/field'
 		}
-	},
-
-	initialize: function() { 
-		console.log('hello good sir');
-		console.log(this.data);
-	},
-
+	}
 });
