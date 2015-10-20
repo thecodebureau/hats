8b4fc9b4f9b01a3352a6aabb74971c16bf5e589b
Linus Miller <lohfu@lohfu.io>
2015-10-20 22:07:05 +0200

    Updated organization to reflect ridge changes (no more crud views).

--- organization/browser/admin/views/organization-page.js
+++ organization/browser/admin/views/organization-page.js
@@ -1,10 +1,36 @@
-
 var app = require('ridge');
 
-module.exports = require('ridge/view').extend({
-	initialize: function(opts) {
-		var _view = this;
+View = require('ridge/view').extend();
 
+_.extend(View.prototype, require('ridge/mixins/observe'), {
+	events: {
+		'submit': 'preventDefault'
+	},
+
+	subviews: {
+		SpytextField: '[data-spytext]',
+		ImageUpload: '.image-upload',
+		ModelControls: '.controls'
+	},
+
+	bindings: {
+		'name': 'value',
+		'legalName': 'value',
+		'email': 'value',
+		'telephone': 'value',
+		'address.streetAddress': 'value',
+		'address.postalCode': 'value',
+		'address.addressLocality': 'value',
+		'address.addressRegion': 'value',
+		'address.addressCountry': 'value'
+	},
+
+	initialize: function(opts) {
+		var id = _.last(window.location.pathname.split('/'));
+
+		var collection = new app.collections.Employees();
+
+		// save page model data
 		this.data = this.model.toJSON();
 
 		this.model = new app.models.Organization();
@@ -13,20 +39,8 @@ module.exports = require('ridge/view').extend({
 	},
 
 	attach: function() {
-		this.formView = new app.views.CrudForm({ 
-			el: this.$('.form'),
-			model: this.model,
-			bindings: {
-				'name': 'value',
-				'legalName': 'value',
-				'email': 'value',
-				'telephone': 'value',
-				'address.streetAddress': 'value',
-				'address.postalCode': 'value',
-				'address.addressLocality': 'value',
-				'address.addressRegion': 'value',
-				'address.addressCountry': 'value'
-			}
-		});
+		this.observe();
 	},
 });
+
+module.exports = View;
