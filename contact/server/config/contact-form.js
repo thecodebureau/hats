095a52cbd4f9030d3293dc2b0f86e9142dc1723a
Linus Miller <lohfu@lohfu.io>
2015-12-10 18:55:55 +0100

    Contact config updates (removed mail config, is now in Epiphany).

--- contact/server/config/contact-form.js
+++ contact/server/config/contact-form.js
@@ -1,15 +1,17 @@
-module.exports = {
-	from: 'no-reply@thecodebureau.com',
-	to: 'info@thecodebureau.com',
-	subject: 'Message from website visitor',
-	error: {
-		type: 'error',
-		heading: 'Oops!',
-		body: 'There was an error sending your email. Please try again or contact the webmaster if the problem persists.'
-	},
-	success: {
-		type: 'success',
-		heading: 'Message sent!',
-		body: 'We will respond to your request as soon as possible'
-	}
+module.exports = function(config) {
+	return {
+		from: 'Robot <' + config.mail.emails.robot + '>',
+		to: config.mail.emails.info,
+		subject: 'Message from website visitor',
+		error: {
+			type: 'error',
+			heading: 'Oops!',
+			body: 'There was an error sending your email. Please try again or contact the webmaster if the problem persists.'
+		},
+		success: {
+			type: 'success',
+			heading: 'Message sent!',
+			body: 'We will respond to your request as soon as possible'
+		}
+	};
 };
